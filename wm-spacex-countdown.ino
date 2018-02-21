3c91597c45ffb6deb1348fee9fa7dbd8978434f2
190n <ben@190n.org>
2018-02-21 09:17:52 -0800

    Work on progress bar

--- wm-spacex-countdown.ino
+++ wm-spacex-countdown.ino
@@ -28,13 +28,17 @@ bool receivingData = false;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 
-char countdownText[12] = "00:00:00:00", oldCountdownText[12] = "__ __ __ __", *payloads;
+char countdownText[14] = "T-00:00:00:00", oldCountdownText[14] = "xxxxxxxxxxxxx", *payloads;
 
 bool payloadsChanged = false;
 
 uint8_t loopCounter = 0;
 
 timespan_t untilNextLaunch;
+
+bool hasWifi = true, refreshing = false, refreshingChanged = false;
+
+uint16_t progressCounter = 0;
 
 Adafruit_HX8357 tft = Adafruit_HX8357(D6, D7, D5, D3, -1, D4);
 
@@ -47,14 +51,46 @@ Adafruit_HX8357 tft = Adafruit_HX8357(D6, D7, D5, D3, -1, D4);
 #define CHAR_GAP 1
 // screen dimensions in pixels
 #define SCREEN_WIDTH 480
-#define SCREEN_HEIGHT 480
+#define SCREEN_HEIGHT 320
 // cursor location to draw countdown
 // X is calculated
-#define CURSOR_X 45
-#define CURSOR_Y 45
+#define CURSOR_X 9
+#define CURSOR_Y 20
 
-#define PAYLOADS_Y 93
+#define PAYLOADS_Y 68
 #define PAYLOADS_SIZE 2
+
+// #999999
+#define ICON_COLOR 0x9cd3
+#define ICON_SIZE 24
+// calculated
+#define ICON_Y 296
+
+// #ffc107
+// material: amber 500
+#define PROGRESS_COLOR 0xfe00
+#define PROGRESS_HEIGHT 4
+
+// from material icons
+// posterized
+// 24x24
+const unsigned char noWifiIcon[] PROGMEM = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x30, 0x00, 0x00, 0x18, 0xff, 0x00, 0x0c,
+    0x7f, 0xe0, 0x1e, 0x3f, 0xf8, 0x7f, 0x1f, 0xfe, 0x7f, 0x8f, 0xfe, 0x3f, 0xc7,
+    0xfc, 0x3f, 0xe3, 0xfc, 0x1f, 0xf1, 0xf8, 0x0f, 0xf8, 0xf0, 0x07, 0xfc, 0x60,
+    0x03, 0xfe, 0x00, 0x03, 0xff, 0x00, 0x01, 0xff, 0x80, 0x00, 0xff, 0xc0, 0x00,
+    0x7e, 0x60, 0x00, 0x3c, 0x30, 0x00, 0x3c, 0x10, 0x00, 0x18, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0
+};
+
+const unsigned char refreshingIcon [] PROGMEM = {
+    0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x0c, 0x00, 0x00, 0x0e, 0x00, 0x00,
+    0x7f, 0x00, 0x01, 0xff, 0x00, 0x03, 0xce, 0x00, 0x07, 0x0c, 0x00, 0x06, 0x08,
+    0x20, 0x0e, 0x00, 0x70, 0x0c, 0x00, 0x30, 0x0c, 0x00, 0x30, 0x0c, 0x00, 0x30,
+    0x0c, 0x00, 0x30, 0x0e, 0x00, 0x70, 0x04, 0x10, 0x60, 0x00, 0x30, 0xe0, 0x00,
+    0x73, 0xc0, 0x00, 0xff, 0x80, 0x00, 0xfe, 0x00, 0x00, 0x70, 0x00, 0x00, 0x30,
+    0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x0
+};
 
 timespan_t::timespan_t() {
     seconds = 0;
@@ -112,13 +148,9 @@ void setup() {
     tft.setCursor(CURSOR_X, CURSOR_Y);
     tft.println(countdownText);
     // so we can open the monitor
-    delay(2000);
-    rect_t r;
-    getDrawBox(1, r);
-    char t[100];
-    sprintf(t, "%d %d %d %d", r.x, r.y, r.w, r.h);
-    Serial.println(t);
+    // delay(2000);
     requestData();
+    startProgress();
 }
 
 void loop() {
@@ -131,6 +163,7 @@ void loop() {
 
     --untilNextLaunch;
     displayCountdown();
+    iterProgress();
 
     loopCounter++;
     // 1 second between loops, accounting for time it took to run loop
@@ -138,11 +171,15 @@ void loop() {
 }
 
 void requestData() {
+    refreshing = true;
+    refreshingChanged = true;
     Serial.println("requesting data");
     Particle.publish("upcomingLaunches");
 }
 
 void gotLaunchData(const char* name, const char* data) {
+    refreshing = false;
+    refreshingChanged = true;
     Serial.println("received launch data");
     Serial.println(data);
     nextLaunchTimestamp = atoi(data);
@@ -151,12 +188,13 @@ void gotLaunchData(const char* name, const char* data) {
     // \________/   \____________________/
     //  timestamp         payloads
     // so we look for the first ampersand, then go forward 2 characters to get the payloads
-    char* payloadsStart = data;
+    const char* payloadStart = data;
     while (*payloadStart != '&') payloadStart++;
     payloadStart += 2;
     if (strcmp(payloadStart, payloads) != 0) payloadsChanged = true;
     // figure out how long the payload info is
     size_t payloadLen = strlen(data) - (payloadStart - data);
+    free(payloads);
     payloads = (char*) malloc(payloadLen * sizeof(char));
     strcpy(payloads, payloadStart);
     timeUntil(nextLaunchTimestamp, untilNextLaunch);
@@ -164,16 +202,16 @@ void gotLaunchData(const char* name, const char* data) {
 
 void displayCountdown() {
     strcpy(oldCountdownText, countdownText);
-    sprintf(countdownText, "%02d:%02d:%02d:%02d", untilNextLaunch.days, untilNextLaunch.hours, untilNextLaunch.minutes, untilNextLaunch.seconds);
+    sprintf(countdownText, "T-%02d:%02d:%02d:%02d", untilNextLaunch.days, untilNextLaunch.hours, untilNextLaunch.minutes, untilNextLaunch.seconds);
     Serial.println(countdownText);
     fastDrawCountdown();
 }
 
 
-// given two countdowns (12 chars each) returns bit mask of which chars differ
+// given two countdowns (13 chars each) returns bit mask of which chars differ
 uint16_t diffCountdowns(char* c1, char* c2) {
     uint16_t result = 0;
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 14; i++) {
         if (c1[i] != c2[i]) {
             result += pow(2, i);
         }
@@ -184,10 +222,8 @@ uint16_t diffCountdowns(char* c1, char* c2) {
 void fastDrawCountdown() {
     uint16_t diff = diffCountdowns(oldCountdownText, countdownText);
     rect_t drawBox;
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 14; i++) {
         if (diff & (int)pow(2, i)) {
-            Serial.print("replacing char ");
-            Serial.println(oldCountdownText[i]);
             getDrawBox(i, drawBox);
             tft.setTextColor(HX8357_BLACK);
             tft.setTextSize(TEXT_SIZE);
@@ -199,6 +235,12 @@ void fastDrawCountdown() {
             tft.print(countdownText[i]);
         }
     }
+    // if (hasWifi != WiFi.ready()) {
+    //     tft.drawBitmap(SCREEN_WIDTH - ICON_SIZE, ICON_Y, noWifiIcon, ICON_SIZE, ICON_SIZE, hasWifi ? HX8357_BLACK : ICON_COLOR);
+    // }
+    if (refreshingChanged) {
+        tft.drawBitmap(SCREEN_WIDTH - ICON_SIZE, ICON_Y, refreshingIcon, ICON_SIZE, ICON_SIZE, refreshing ? ICON_COLOR : HX8357_BLACK);
+    }
     if (!payloadsChanged) return;
     // not as optimized as it could be but I'm lazy
     tft.fillRect(CURSOR_X, PAYLOADS_Y, SCREEN_WIDTH - CURSOR_X, CHAR_HEIGHT * PAYLOADS_SIZE, HX8357_BLACK);
@@ -206,4 +248,29 @@ void fastDrawCountdown() {
     tft.setTextSize(PAYLOADS_SIZE);
     tft.setCursor(CURSOR_X, PAYLOADS_Y);
     tft.print(payloads);
+    payloadsChanged = false;
+}
+
+void startProgress() {
+    progressCounter = 0;
+    tft.fillRect(0, SCREEN_HEIGHT - PROGRESS_HEIGHT, SCREEN_WIDTH, PROGRESS_HEIGHT, HX8357_BLACK);
+    for (int i = 0; i < PROGRESS_HEIGHT; i++) {
+        int y = (SCREEN_HEIGHT - PROGRESS_HEIGHT) + i;
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            if ((x + i) % 120 < 90) tft.drawPixel(x, y, PROGRESS_COLOR);
+        }
+    }
+}
+
+void iterProgress() {
+    progressCounter++;
+    if (progressCounter >= 120) progressCounter = 0;
+    for (int row = 0; row < PROGRESS_HEIGHT; row++) {
+        int y = (SCREEN_HEIGHT - PROGRESS_HEIGHT) + row;
+        for (int x = (progressCounter + 89 - row); x < SCREEN_WIDTH; x += 120) tft.drawPixel(x, y, PROGRESS_COLOR);
+    }
+}
+
+void stopProgress() {
+    tft.fillRect(0, SCREEN_HEIGHT - PROGRESS_HEIGHT, SCREEN_WIDTH, PROGRESS_HEIGHT, HX8357_BLACK);
 }
